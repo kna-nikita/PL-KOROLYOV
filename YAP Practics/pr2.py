234bf865320fde7a50f4631d8fa594f40698a138
kna-nikita <n.k.381@yandex.ru>
2022-09-20 17:38:32 +0300

    Create pr2.py

---
+++ YAP Practics/pr2.py
@@ -0,0 +1,20 @@
+import math
+
+x = float(input())
+y = float(input())
+z = float(input())
+
+e = math.e
+c = x - y
+c1 = x + y
+
+a = math.pow(e, abs(c)) * math.pow(abs(c), c1)
+
+b = math.atan(x) + math.atan(z)
+
+u = x**6 + math.log(e, y)**2
+
+t = math.pow(u, 1/3)
+
+w = (a / b) + t
+print(w) 
